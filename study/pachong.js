a5101a7b0492e89a6b6cbe3817d4dd8e185a0fb3
mrma <mtf986252757@gmail.com>
2017-06-05 23:51:21 +0800

    update web crawler plug-in

--- study/pachong.js
+++ study/pachong.js
@@ -1,20 +1,37 @@
 var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+
+var originUrl = 'https://www.douyu.com/directory/game/CSGO';
+var originUrl1 = 'http://www.qiushibaike.com/text';
+var originUrl2 = 'http://www.qiushibaike.com';
 var options = {
-  url: 'http://www.qiushibaike.com/',
+  url: originUrl,
   headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3107.5 Safari/537.36'
   }
 };
 request(options, function(err, res, body) {
-  var arr = /<a[^>]+>/.exec(body);
-  arr = body.match(/<div class=.content[^-]+[^>]+\/div>/gi);
+  var $ = cheerio.load(body, {
+    ignoreWhitespace: true,
+    xmlMode: true
+  });
+  var lists = $('#live-list-contentbox .play-list-link');
+  var results = [];
 
-  for (var i = 0; i < arr.length; i++) {
-    arr[i] = arr[i].match(/<span>.+<\/span>/)[0];
-    arr[i] = arr[i].replace(/<br\/>/g, '');
-    arr[i] = arr[i].replace(/<span>/, '');
-    arr[i] = arr[i].replace(/<\/span>/, '');
-  }
-  console.log(arr.length);
-  console.log(arr);
+  var writer = fs.createWriteStream('./dota2 data.txt');
+  lists.each(function(index, elem) {
+    var $this = $(this);
+    writer.write('标题：【' + $this.find('.mes h3').text() + '】\n', 'UTF8');
+    writer.write('主播：【' + $this.find('.dy-name').text() + '】  人数：【' + $this.find('.dy-num').text() + '】\n', 'UTF8');
+    writer.write('\n', 'UTF8');
+    results.push({
+      html: $this.html(),
+      title: $this.find('.mes h3').text(),
+      name: $this.find('.dy-name').text(),
+      num: $this.find('.dy-num').text()
+    });
+  });
+
+  writer.end();
 });
