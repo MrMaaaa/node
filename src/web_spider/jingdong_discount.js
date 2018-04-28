7a52f557c5af0b69091fa225910ffa3825bd0bee
mac <mac@macdeMacBook-Pro.local>
2018-04-28 17:23:21 +0800

    京东优惠券使用puppeteer.js进行爬取

--- src/web_spider/jingdong_discount.js
+++ src/web_spider/jingdong_discount.js
@@ -1,44 +1,67 @@
-const request = require('request');
-const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 const fs = require('fs');
-const iconv = require('iconv-lite');
 
-const HOME_PRE = 'https://a.jd.com';
+const PAGE = 'https://a.jd.com';
 
-const options = {
-  url: HOME_PRE,
-  headers: {
-    'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3107.5 Safari/537.36'
-  },
-  encoding: null
-};
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+  const page = await browser.newPage();
+
+  await page.goto(PAGE);
+
+  await page.evaluate((body) => {
+    document.documentElement.scrollTop = document.body.clientHeight;
+  });
+
+  await page.content();
+
+  const body = await page.$('body');
+  const results = await page.evaluate((body) => {
+    document.documentElement.scrollTop = document.body.clientHeight;
+    return {
+      body: [...body.querySelectorAll('#quanlist > .quan-item')].map((item) => {
+        return {
+          name: item.querySelector('.q-price').innerText,
+          range: item.querySelector('.q-range').innerText,
+          process:
+            item.querySelector('.q-progress') &&
+            item.querySelector('.q-progress').innerText
+        };
+      })
+    };
+  }, body);
+  console.dir(results.body.length);
+
+  await browser.close();
+})();
 
 //京东需先解决登录问题
-request(options, function(err, res, body) {
-  const $ = cheerio.load(body, {
-    ignoreWhitespace: true,
-    xmlMode: true
-  });
+// request(options, function(err, res, body) {
+//   const $ = cheerio.load(body, {
+//     ignoreWhitespace: true,
+//     xmlMode: true
+//   });
 
-  const lists = [];
-  const results = $('body');
-  results.each(function(index, elem) {
-    lists.push({
-      price: $(this)
-        .find('.q-type .q-price strong')
-        .text(),
-      limit: $(this)
-        .find('.q-type .q-price .q-limit')
-        .text(),
-      range: $(this)
-        .find('.q-type .q-range')
-        .text(),
-      process: $(this)
-        .find('.q-type .q-ops-box .q-progress .txt')
-        .text()
-    });
-  });
+//   const lists = [];
+//   const results = $('body');
+//   results.each(function(index, elem) {
+//     lists.push({
+//       price: $(this)
+//         .find('.q-type .q-price strong')
+//         .text(),
+//       limit: $(this)
+//         .find('.q-type .q-price .q-limit')
+//         .text(),
+//       range: $(this)
+//         .find('.q-type .q-range')
+//         .text(),
+//       process: $(this)
+//         .find('.q-type .q-ops-box .q-progress .txt')
+//         .text()
+//     });
+//   });
 
-  console.log(results.text());
-});
+//   console.log(results.text());
+// });
