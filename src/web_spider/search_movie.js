7dfacb470f6ef13a30cff862a5da43d843e522dd
matengfei <matengfei@MrMa-2.local>
2020-05-25 15:10:58 +0800

    删除了旧的搜索电影脚本，使用资源更全的新网站，并支持导出下载地址

--- src/web_spider/search_movie.js
+++ src/web_spider/search_movie.js
@@ -1,32 +1,85 @@
+const readline = require('readline');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const iconv = require('iconv-lite');
 
-// 获取输入的电影名字
-const movie_name = process.argv[2] || '';
+const IS_HEADLESS = true;
 
-// 将名字转为gbk编码
-const search = iconv.encode(movie_name, 'gbk').toString('hex').match(/.{2}/g).map((el) => '%' + el).join('').toLocaleUpperCase();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const SEARCH_URL = `http://s.ygdy8.com/plus/so.php?kwtype=0&searchtype=title&keyword=${search}`;
-const HOME_URL = `http://www.ygdy8.com`;
+rl.on('close', () => {});
 
-// TODO
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: true
+const getInput = (readline, question) => {
+  return new Promise((resolve, reject) => {
+    readline.question(question, (input) => {
+      resolve(input);
+    });
   });
-  const page = await browser.newPage();
+};
 
-  await page.goto(SEARCH_URL);
+getInput(rl, '请输入电影名字：').then((name) => {
+  searchMovie(name, rl);
+});
 
-  await page.content();
-
-  const searchContent = await page.$eval('body', (body) => {
-    return {
-      link: body.querySelectorAll('.co_content8 td b a')
-    };
-  });
-
-  await browser.close();
-})();
+const searchMovie = async (movieName = '', readline) => {
+  try {
+    const browser = await puppeteer.launch({
+      headless: IS_HEADLESS,
+    });
+    const page = await browser.newPage();
+    // 这里如果把url改成变量即报错，原因未知，下同
+    await page.goto('https://www.dy2018.com');
+    await page.content();
+    await page.type('.formhue', movieName, { delay: 100 });
+    const searchPagePromise = new Promise((res) =>
+      browser.once('targetcreated', (tag) => res(tag.page()))
+    );
+    await page.tap('.searchr input');
+    const searchPage = await searchPagePromise;
+    await searchPage.content();
+    await searchPage.waitFor(1000);
+    const searchContent = await searchPage.$$eval('b > a', (res) =>
+      res.map((item) => ({
+        href: `https://www.dy2018.com${item.getAttribute('href')}`,
+        name: item.getAttribute('title'),
+      }))
+    );
+    console.log('结果：');
+    console.dir(searchContent.map((item, index) => `${index}: ${item.name}`));
+    if (searchContent.length === 0) {
+      console.log('无搜索结果');
+      await browser.close();
+      process.exit(0);
+    }
+    const index = await getInput(readline, '请输入序号：');
+    await searchPage.goto(searchContent[index].href, {
+      waitUntil: 'domcontentloaded',
+      timeout: 3000,
+    });
+    await searchPage.content();
+    const indexContent = await searchPage.$$eval('#Zoom table td a', (res) => {
+      return res.map((item) => ({
+        thunder: item.getAttribute('thunderhref'),
+        ftp: item.innerText,
+      }));
+    });
+    console.log('下载地址：');
+    console.dir(indexContent);
+    const isWriteToFile = await getInput(readline, '是否保存到本地？（Y/N）');
+    if (isWriteToFile === 'Y' || isWriteToFile === 'y') {
+      const writer = fs.createWriteStream(`./${movieName} 下载地址.txt`);
+      indexContent.map((item) => {
+        writer.write(`迅雷：${item.thunder}\n`, 'UTF8');
+        writer.write(`FTP：${item.ftp}\n`, 'UTF8');
+      });
+      console.log('写入完成');
+    }
+    await browser.close();
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+    process.exit(0);
+  }
+};
